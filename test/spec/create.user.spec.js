dd43fd979e9ea4ee8076b8c2920b0cbab2bf7923
alvintri <alvin.trianto@evermos.com>
2023-07-30 20:40:12 +0700

    Add mochewsome report

--- test/spec/create.user.spec.js
+++ test/spec/create.user.spec.js
@@ -1,6 +1,4 @@
 const request = require("supertest");
-const { expect } = require("chai");
-const { getToken } = require("./get.token.spec");
 const config = require('../../data/config.json')
 
 async function createUser(payload,token){
@@ -11,20 +9,5 @@ async function createUser(payload,token){
     return response
 }
 
-describe('Create User', () => {
-    it('Succcess create a new user', async () => {
-        //Get Token
-        const token = await getToken()
-        //Create User
-        const payload = {
-            "name": "kasir-serbaguna",
-            "email": "user@example.com",
-            "password": "jiasda2321@"
-         }
-         
-        const response = await createUser(payload,token)
-        //Assert
-        expect((await response).status).to.equal(201);
-        expect((await response).body.data.name).to.equal(payload.name); //DATA DRIVEN TESTING
-    })
-})
+module.exports = {createUser}
+
